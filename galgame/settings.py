3a15d91f0376fc83ebd768f3eb4442b6861f2ed2
Ancaratsi <hana77321@outlook.com>
2025-03-24 16:15:47 +0800

    第一版

--- galgame/settings.py
+++ galgame/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z9+7mm(#46e_%psn%8zw3*4__i*%@wm0cpatsqo$5o2lg4nup-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['galgame.space', 'www.galgame.space','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','galgame.space', 'www.galgame.space','127.0.0.1']
 
 
 # Application definition
